76b06e74519a98741200153444a7eec8571ae466
Dillon Stock <stockdil@msu.edu>
2018-05-30 14:40:49 -0400

    Added downloads/contact/blog html pages.

--- personal/urls.py
+++ personal/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 from django.conf.urls import url
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
-    url('^$', views.index, name='index'),
-    path('personal/', views.index, include('personal.urls')),
+    #url('^$', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('downloads/', views.downloads),
+    path('downloads/resume.pdf/', views.download_resume),
+    path('contact/', views.contact),
+    path('blog/', views.blog),
+    #path('personal/', views.index, include('personal.urls')),
 ]
--- personal/views.py
+++ personal/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from wsgiref.util import FileWrapper
+import os
 
 def index(request):
     return render(request, 'personal/home.html')
+
+def downloads(request):
+    return render(request, 'personal/downloads.html')
+
+def contact(request):
+    return render(request, 'personal/contact.html')
+
+def blog(request):
+    return render(request, 'personal/blog.html')
+
+def download_resume(request):
+    filename = 'Dillon_Stock_Resume_2018.pdf'
+    content = FileWrapper(filename)
+    response = HttpResponse(content, content_type='application/pdf')
+    response['Content-Length'] = os.path.getsize(filename)
+    response['Content-Disposition'] = 'attachment; filename=%s' % 'Dillon_Stock_Resume_2018.pdf'
+    return response
