c300a7dd0eb566b292ddd5ac139333fb2a79920d
Ubuntu <ubuntu@ip-172-31-27-142.ec2.internal>
2018-08-04 17:37:59 +0000

    Added STATIC_ROOT = '/home/ubuntu/django_projects/static' to settings.py. Updated location of static files in /etc/nginx/sites-available/mysite.conf. IMPORTANT: must run command 'python manage.py collectstatic' after adding static files for them to be accessible by the nginx web server.

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,5 +136,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
 ]
 
+STATIC_ROOT = "/home/ubuntu/django_projects/static/"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'object_detection/media')
